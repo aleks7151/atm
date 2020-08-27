d33f3120f10ba96067886a1785a18d8273065400
aleks7151 <s_sasha_2000@mail.ru>
2020-08-27 15:51:33 +0300

    kostyl

--- src/main/java/ru/sbrf/back_office/Authentication.java
+++ src/main/java/ru/sbrf/back_office/Authentication.java
@@ -1,39 +1,14 @@
 package ru.sbrf.back_office;
 
-import ru.sbrf.atm.User;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class Authentication {
     private String numberCard;
     private String scanerFinger;
     private String clock;
 
-    public Authentication(User user, String numberCard, String scanerFinger, String clock){
+    public Authentication(String numberCard, String scanerFinger, String clock){
         this.numberCard = validateNC(numberCard);
         this.scanerFinger = validateSF(scanerFinger);
         this.clock = validateC(clock);
-        try {
-            Map<String, User> map = getMyHashMap();
-            map.put(getKey(), user);
-        }
-        catch (Exception e){System.out.println("No(");}
-    }
-
-    private String getKey(){
-        String numberCard = "", scanerFinger = "", clock = "";
-        if (this.numberCard != null)
-            numberCard = this.numberCard;
-        if (this.scanerFinger != null)
-            scanerFinger = this.scanerFinger;
-        if (this.clock != null)
-            clock = this.clock;
-        return numberCard + "|" + scanerFinger + "|" + clock;
-    }
-
-    private Map<String, User> getMyHashMap() throws Exception{
-        return new HashMap<String, User>();
     }
 
     private String validateNC(String numberCard){
