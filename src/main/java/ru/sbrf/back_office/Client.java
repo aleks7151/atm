799481f5eba7a4864aeb45a92cf1ba3d17c8806b
aleks7151 <s_sasha_2000@mail.ru>
2020-09-03 14:54:00 +0300

    <>

--- src/main/java/ru/sbrf/back_office/Client.java
+++ src/main/java/ru/sbrf/back_office/Client.java
@@ -1,8 +1,12 @@
 package ru.sbrf.back_office;
 
-public class Client {
+import java.util.Map;
+
+public class Client<P> {
     private Account account;
     private int solvency;
+    Map<String,P> accS;
+    Map<String,P> accC;
 
 
     public Client(){
@@ -20,5 +24,15 @@ public class Client {
     public Client(Account account, int solvency){
         this.account = account;
         this.solvency = solvency;
+        save(accS);
+        current(accC);
+    }
+
+    private void save(Map<String, P> accS){
+        //Действия
+    }
+
+    private void current(Map<String, P> accC){
+        //Действия
     }
 }
